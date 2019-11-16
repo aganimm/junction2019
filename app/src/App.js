1ffbe7ff696132c592ce94babb687d1b4395ce90
bixnel <hi@bixnel.ru>
2019-11-16 22:53:15 +0300

    Make Tinder great again!

--- app/src/App.js
+++ app/src/App.js
@@ -1,86 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import connect from '@vkontakte/vk-connect';
+import View from '@vkontakte/vkui/dist/components/View/View';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
-import { Epic, ScreenSpinner, Tabbar, TabbarItem } from '@vkontakte/vkui';
 
-import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
-import Icon28Favorite from '@vkontakte/icons/dist/28/favorite';
-import Icon28Profile from '@vkontakte/icons/dist/28/profile';
-
-import Feed from './tabs/Feed';
-import Favorites from './tabs/Favorites';
-import UserCache from './UserCache';
+import Home from './panels/Home';
+import Persik from './panels/Persik';
 
 const App = () => {
-	const [activeStory, setActiveStory] = useState('feed');
+	const [activePanel, setActivePanel] = useState('home');
 	const [fetchedUser, setUser] = useState(null);
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
 	useEffect(() => {
 		connect.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = 'client_light';//data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-			if (type === 'VKWebAppAccessTokenReceived') {
-				const { access_token: accessToken } = data;
-				UserCache._it.setAccessToken(accessToken);
-				UserCache._it.refreshMiniAppToken();
+			switch (type) {
+				case 'VKWebAppUpdateConfig':
+					const schemeAttribute = document.createAttribute('scheme');
+					schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+					document.body.attributes.setNamedItem(schemeAttribute);
+					break;
+				default:
+					console.log('new message', type, data);
 			}
 		});
-		async function fetchData() {
-			connect.send("VKWebAppGetAuthToken", {"app_id": 7210035, "scope": "friends,status"});
-			const user = await connect.sendPromise('VKWebAppGetUserInfo');
-			setUser(user);
 
-			const { id } = user;
-			UserCache._it.setUserId(id);
-			UserCache._it.refreshMiniAppToken();
-
-			setPopout(null);
-		}
-		fetchData();
+		connect
+			.sendPromise('VKWebAppGetUserInfo')
+			.then(data => {
+				console.log('success', data);
+				setUser(data);
+				setPopout(null);
+			})
+			.catch(error => {
+				console.log('error');
+			});
 	}, []);
 
-	const onStoryChange = e => {
-		setActiveStory(e.currentTarget.dataset.story);
-		connect.send('VKWebAppTapticSelectionChanged');
+	const parseQueryString = (string) => {
+        return string.slice(1).split('&')
+            .map((queryParam) => {
+                let kvp = queryParam.split('=');
+                return {key: kvp[0], value: kvp[1]}
+            })
+            .reduce((query, kvp) => {
+                query[kvp.key] = kvp.value;
+                return query
+            }, {})
+    };
+
+
+	const go = e => {
+		setActivePanel(e.currentTarget.dataset.to);
 	};
 
 	return (
-		<Epic activeStory={ activeStory } tabbar={
-			<Tabbar>
-				<TabbarItem
-					onClick={ onStoryChange }
-					selected={ activeStory === 'feed' }
-					data-story='feed'
-					text='Лента'
-				>
-					<Icon28Newsfeed />
-				</TabbarItem>
-				<TabbarItem
-					onClick={ onStoryChange }
-					selected={ activeStory === 'favorites' }
-					data-story='favorites'
-					text='Избранное'
-				>
-					<Icon28Favorite />
-				</TabbarItem>
-				<TabbarItem
-					onClick={ onStoryChange }
-					selected={ activeStory === 'profile' }
-					data-story='profile'
-					text='Профиль'
-				>
-					<Icon28Profile />
-				</TabbarItem>
-			</Tabbar>
-		}>
-			<Feed id='feed' />
-			<Favorites id='favorites' />
-		</Epic>
+		<View activePanel={activePanel} popout={popout}>
+			<Home id='home' fetchedUser={fetchedUser} go={go} />
+			<Persik id='persik' go={go} />
+		</View>
 	);
-};
+}
 
 export default App;
+
