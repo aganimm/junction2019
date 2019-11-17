e16d60d4ffea78531b56f5b119880b01acee727d
isinelnikov <kingreeve@mail.ru>
2019-11-17 05:15:02 +0200

    Backend in frontend.

--- app/src/App.js
+++ app/src/App.js
@@ -1,48 +1,136 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import connect from '@vkontakte/vk-connect';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
+import {
+  Epic,
+  Panel,
+  PanelHeader,
+  ScreenSpinner,
+  Tabbar,
+  TabbarItem,
+  View
+} from '@vkontakte/vkui';
 
+import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
+import Icon28Search from '@vkontakte/icons/dist/28/search';
+import Icon28Messages from '@vkontakte/icons/dist/28/message';
+import Icon28Notifications from '@vkontakte/icons/dist/28/notification';
+import Icon28More from '@vkontakte/icons/dist/28/more';
 import Tutorial from './panels/Tutorial';
-import './fonts.css';
+import Swipeable from 'react-swipy';
+import Card from './component/Card';
 
-const App = () => {
-	const [activePanel, ] = useState('tutorial');
-	const [, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+const wrapperStyles = { position: 'relative', width: '250px', height: '250px' };
 
-	useEffect(() => {
-		connect.subscribe(({ detail: { type, data }}) => {
-			switch (type) {
-				case 'VKWebAppUpdateConfig':
-					const schemeAttribute = document.createAttribute('scheme');
-					schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-					document.body.attributes.setNamedItem(schemeAttribute);
-					break;
-				default:
-					console.log('new message', type, data);
-			}
-		});
+const appStyles = {
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  minHeight: '100vh',
+  fontFamily: 'sans-serif',
+  overflow: 'hidden'
+};
 
-		connect
-			.sendPromise('VKWebAppGetUserInfo')
-			.then(data => {
-				console.log('success', data);
-				setUser(data);
-				setPopout(null);
-			})
-			.catch(error => {
-				console.log('error');
-			});
-	}, []);
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
 
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Tutorial id='tutorial' title='Alinder' />
-		</View>
-	);
+    this.state = {
+      cards: ['First', 'Second', 'Third'],
+      activeStory: 'tutorial',
+      currentScreen: 'tutorial'
+    };
+    this.onStoryChange = this.onStoryChange.bind(this);
+  }
+
+  onStoryChange (e) {
+    this.setState({ activeStory: e.currentTarget.dataset.story })
+  }
+
+  updateCurrentScreen = () => {
+    this.setState({
+      currentScreen: 'main'
+
+    }, () => {
+      this.setState({
+        activeStory: 'feed'
+      })
+    })
+  }
+
+  remove = () =>
+    this.setState(({ cards }) => ({ cards: cards.slice(1, cards.length) }));
+
+  render () {
+    const { cards } = this.state;
+
+    const main = <Epic activeStory={ this.state.activeStory } tabbar={
+      <Tabbar>
+        <TabbarItem
+          onClick={ this.onStoryChange }
+          selected={ this.state.activeStory === 'feed' }
+          data-story="feed"
+          text="Новости"
+
+        ><Icon28Newsfeed/></TabbarItem>
+        <TabbarItem
+          onClick={ this.onStoryChange }
+          selected={ this.state.activeStory === 'discover' }
+          data-story="discover"
+          text="Поиск"
+        ><Icon28Search/></TabbarItem>
+        <TabbarItem
+          onClick={ this.onStoryChange }
+          selected={ this.state.activeStory === 'messages' }
+          data-story="messages"
+          text="Сообщения"
+        ><Icon28Messages/></TabbarItem>
+      </Tabbar>
+    }>
+      <View id="feed" activePanel="feed">
+        <Panel id="feed">
+          <PanelHeader>Профиль</PanelHeader>
+        </Panel>
+      </View>
+      <View id="discover" activePanel="discover">
+        <Panel id="discover">
+          <PanelHeader>Пары</PanelHeader>
+          <div className={ appStyles }>
+            <div className={ appStyles }>
+              { cards.length > 0 && (
+                <div style={ wrapperStyles }>
+                  <Swipeable onAfterSwipe={ this.remove }>
+                    <Card>{ cards[0] }</Card>
+                  </Swipeable>
+                  { cards.length > 1 &&
+                  <Card zIndex={ -1 }>{ cards[1] }</Card> }
+                </div>
+              ) }
+              { cards.length <= 1 && <Card zIndex={ -2 }>No more cards</Card> }
+            </div>
+          </div>
+        </Panel>
+      </View>
+      <View id="messages" activePanel="messages">
+        <Panel id="messages">
+          <PanelHeader>Избранное</PanelHeader>
+        </Panel>
+      </View>
+    </Epic>;
+
+    const tutorial = <Epic activeStory={ this.state.activeStory }>
+      <View id='tutorial' activePanel="tutorial">
+        <Tutorial id='tutorial' title='Alinder'
+                  onClick={ () => {this.updateCurrentScreen()} }/>
+      </View>
+    </Epic>;
+
+    const shotTutorial = this.state.currentScreen === 'tutorial';
+    return (
+      shotTutorial ? tutorial : main
+    )
+  }
 }
-
-export default App;
 
--- app/src/panels/Tutorial.js
+++ app/src/panels/Tutorial.js
@@ -2,102 +2,104 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 
-import Slider from "react-slick";
+import Slider from 'react-slick';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Tutorial.css';
 
 import Icon24Back from '@vkontakte/icons/dist/24/back';
-import img_1 from'../img/1.png';
-import img_2 from'../img/2.png';
-import img_3 from'../img/3.png';
-import img_4 from'../img/4.png';
-import ScreenSpinner from "@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner";
+import img_1 from '../img/1.png';
+import img_2 from '../img/2.png';
+import img_3 from '../img/3.png';
+import img_4 from '../img/4.png';
 
-const Tutorial = ({ id, title }) => {
-	const [slideNum, setSlideNum] = useState(null);
+const Tutorial = ({ id, title, onClick }) => {
+  const [slideNum, setSlideNum] = useState(null);
 
-	return (
-		<Panel id={id}>
-			<div className='title'>
-				<svg width='148' height='40' viewBox='0 0 148 40' fill='none' xmlns='http://www.w3.org/2000/svg'>
-					<rect x='27' y='14' width='121' height='16' fill='black'/>
-					<rect x='19' y='8' width='78' height='2' fill='black'/>
-					<rect x='3' y='8' width='8' height='2' fill='black'/>
-					<rect x='11' y='14' width='8' height='2' fill='black'/>
-					<rect x='21' y='18' width='8' height='2' fill='black'/>
-					<rect x='20' y='28' width='11' height='2' fill='black'/>
-					<rect y='22' width='18' height='2' fill='black'/>
-				</svg>
-				<h1>{title}</h1>
-			</div>
-			<div className="slider_block">
-				<div className="images slide_2">
-					<div className="image img_1">
-						<img src={img_1} alt="img" />
-					</div>
-					<div className="image img_2">
-						<img src={img_2} alt="img" />
-					</div>
-					<div className="image img_3">
-						<img src={img_3} alt="img" />
-					</div>
-					<div className="image img_4">
-						<img src={img_4} alt="img" />
-					</div>
-				</div>
-				<Slider {...{
-					dots: true,
-					infinite: false,
-					arrows: false,
-					beforeChange: (prevIndex, nextIndex) => {
-						console.log(prevIndex, nextIndex);
-						setSlideNum(nextIndex);
-					}
-				}} className='slider'>
-					<div className="slide">
-						<div className="slide_title">
-							Content
-						</div>
-						<div className="slide_hr"/>
-						<div className="slide_text">
-							Bla bla bla
-						</div>
-					</div>
-					<div className="slide">
-						<div className="slide_title">
-							Как?
-						</div>
-						<div className="slide_hr"/>
-						<div className="slide_text">
-							Вы еще не слышали про Alinder?
-						</div>
-					</div>
-					<div className="slide">
-						<div className="slide_title">
-							Вот так
-						</div>
-						<div className="slide_hr"/>
-						<div className="slide_text">
-							Даб даб даб
-						</div>
-					</div>
-				</Slider>
-				<div className="buttons">
-					<div className={"skip " + (slideNum === 2)}>Skip</div>
-					<div className={"next " + (slideNum === 2)}
-					onClick={() => {  }}>
-						<Icon24Back />
-					</div>
-				</div>
-			</div>
-		</Panel>
-	);
+  return (
+    <Panel id={ id }>
+      <div className='title'>
+        <svg width='148' height='40' viewBox='0 0 148 40' fill='none'
+             xmlns='http://www.w3.org/2000/svg'>
+          <rect x='27' y='14' width='121' height='16' fill='black'/>
+          <rect x='19' y='8' width='78' height='2' fill='black'/>
+          <rect x='3' y='8' width='8' height='2' fill='black'/>
+          <rect x='11' y='14' width='8' height='2' fill='black'/>
+          <rect x='21' y='18' width='8' height='2' fill='black'/>
+          <rect x='20' y='28' width='11' height='2' fill='black'/>
+          <rect y='22' width='18' height='2' fill='black'/>
+        </svg>
+        <h1>{ title }</h1>
+      </div>
+      <div className="slider_block">
+        <div className="images slide_2">
+          <div className="image img_1">
+            <img src={ img_1 } alt="img"/>
+          </div>
+          <div className="image img_2">
+            <img src={ img_2 } alt="img"/>
+          </div>
+          <div className="image img_3">
+            <img src={ img_3 } alt="img"/>
+          </div>
+          <div className="image img_4">
+            <img src={ img_4 } alt="img"/>
+          </div>
+        </div>
+        <Slider { ...{
+          dots: true,
+          infinite: false,
+          arrows: false,
+          beforeChange: (prevIndex, nextIndex) => {
+            console.log(prevIndex, nextIndex);
+            setSlideNum(nextIndex);
+          }
+        } } className='slider'>
+          <div className="slide">
+            <div className="slide_title">
+              Content
+            </div>
+            <div className="slide_hr"/>
+            <div className="slide_text">
+              Bla bla bla
+            </div>
+          </div>
+          <div className="slide">
+            <div className="slide_title">
+              Как?
+            </div>
+            <div className="slide_hr"/>
+            <div className="slide_text">
+              Вы еще не слышали про Alinder?
+            </div>
+          </div>
+          <div className="slide">
+            <div className="slide_title">
+              Вот так
+            </div>
+            <div className="slide_hr"/>
+            <div className="slide_text">
+              Даб даб даб
+            </div>
+          </div>
+        </Slider>
+        <div className="buttons">
+          <div className={ 'skip ' + (slideNum === 2) } onClick={() => onClick()}>Skip</div>
+          <div className={ 'next ' + (slideNum === 2) }
+               onClick={ () => {
+               	console.log('on click');
+               	onClick()} }>
+            <Icon24Back/>
+          </div>
+        </div>
+      </div>
+    </Panel>
+  );
 };
 
 Tutorial.propTypes = {
-	id: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 
 };
 
